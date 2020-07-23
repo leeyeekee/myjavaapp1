39867bee6ede01c660ffce58eec11ea298dcee69
admin <leeyeekee@hotmail.com>
2020-07-23 11:37:14 +0800

    added changes to simulate cve scan

--- src/main/java/io/openshift/booster/service/GreetingController.java
+++ src/main/java/io/openshift/booster/service/GreetingController.java
@@ -33,6 +33,11 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
         return new Greeting(String.format(properties.getMessage(), name));
     }
 }
